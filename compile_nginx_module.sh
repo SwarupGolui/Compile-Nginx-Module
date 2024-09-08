6b19a6c188a77f02c795dcce3ad631dd74fc6069
Swarup Golui <swarupgolui00@gmail.com>
2024-09-08 19:28:34 +0530

    Update compile_nginx_module.sh
    fix brotili

--- compile_nginx_module.sh
+++ compile_nginx_module.sh
@@ -15,7 +15,13 @@ cd ./
 wget "https://nginx.org/download/nginx-$nginx_version.tar.gz" -O - | tar -xz
 
 # brotili
-git clone --recurse-submodules https://github.com/google/ngx_brotli.git
+# git clone --recurse-submodules https://github.com/google/ngx_brotli.git
+git clone --recurse-submodules -j8 https://github.com/google/ngx_brotli
+cd ngx_brotli/deps/brotli
+mkdir out && cd out
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
+cmake --build . --config Release --target brotlienc
+cd ../../../..
 
 # headers-more
 git clone https://github.com/openresty/headers-more-nginx-module.git
